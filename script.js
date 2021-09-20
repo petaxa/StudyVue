8c8037008bb681f25ca827f6cb243e5fc937fb45
tempestif <soccer.i_y@icloud.com>
2021-09-21 08:20:42 +0900

    until computed

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 const Counter = {
-    data(){
-        return{
-            counter:0,
-            message:new Date()
+    data() {
+        return {
+            counter: 0,
+            message: new Date()
         }
     },
-    mounted(){
-        setInterval(() =>{
+    mounted() {
+        setInterval(() => {
             this.counter++
-        },1000)
+        }, 1000)
     }
 }
 Vue.createApp(Counter).mount('#counter')
 
 const AttributeBinding = {
-    data(){
+    data() {
         return {
-            message: 'You loaded this page on'+new Date().toLocaleString()
+            message: 'You loaded this page on' + new Date().toLocaleString()
         }
     }
 }
 Vue.createApp(AttributeBinding).mount('#bind-attribute')
 
 const EventHanding = {
-    data(){
-        return{
-            message:'Hello vue.js!',
+    data() {
+        return {
+            message: 'Hello vue.js!',
             isStatus: true,
-            status:'nomal'
+            status: 'nomal'
         }
     },
-    methods:{
-        reverseMessage(){
-            this.isStatus = !this.isStatus 
+    methods: {
+        reverseMessage() {
+            this.isStatus = !this.isStatus
             this.message = this.message
-            .split('')
-            .reverse()
-            .join('')
+                .split('')
+                .reverse()
+                .join('')
             // if(this.isStatus == false){
             //     this.status = 'reverse'
             // }else{
@@ -51,13 +51,13 @@ const EventHanding = {
 Vue.createApp(EventHanding).mount('#event-handling')
 
 const ContidionalRendering = {
-    data(){
-        return{
-            seen:true
+    data() {
+        return {
+            seen: true
         }
     },
-    methods:{
-        toggleSeen(){
+    methods: {
+        toggleSeen() {
             this.seen = !this.seen
         }
     }
@@ -65,14 +65,14 @@ const ContidionalRendering = {
 Vue.createApp(ContidionalRendering).mount('#conditional-rendering')
 
 const ListRendering = {
-    data(){
+    data() {
         return {
-            todos:[
-                {text:'Learn JS'},
-                {text:'Learn Vue'},
-                {text:'Build something awesome'},
-                {color:'red'},
-                {color:'blue'}
+            todos: [
+                { text: 'Learn JS' },
+                { text: 'Learn Vue' },
+                { text: 'Build something awesome' },
+                { color: 'red' },
+                { color: 'blue' }
             ]
         }
     }
@@ -80,43 +80,78 @@ const ListRendering = {
 Vue.createApp(ListRendering).mount('#list-rendering')
 
 const ComponentsApp = {
-    data(){
-        return{
-            groceryList:[
-                {id:0,text:'vegetables'},
-                {id:1,text:'cheese'},
-                {id:2,text:'whatever else humans are supposed to eat'}
+    data() {
+        return {
+            groceryList: [
+                { id: 0, text: 'vegetables' },
+                { id: 1, text: 'cheese' },
+                { id: 2, text: 'whatever else humans are supposed to eat' }
             ]
         }
     }
 }
 
+//テンプレート構文
+//コンポーネントの登録より後ろにやったら動かなかった。
+//createAppのみだと動く。
+const RenderTemplate = {
+    data() {
+        return {
+            msg: 'メッセージ',
+            rawHtml: '<span style="color: red">This should be red.</span>',
+            dynamicId: 'add',
+            isButtonDisabled: false,
+            number:1000
+        }
+    }
+}
+Vue.createApp(RenderTemplate).mount('#template-syntax')
+
+Vue.createApp({
+    data(){
+        return{
+            author: {
+                name:'John',
+                books:[
+                    'vue1',
+                    'vue2',
+                    'vue3'
+                ]
+            }
+        }
+    },
+    computed: {
+        publishedBookMessage(){
+            return this.author.books.length > 0 ? 'yes' : 'no'
+        }
+    }
+}).mount('#computed-basics')
 
 //アプリケーションとコンポーネント
 //アプリケーションインスタンスを作成
 const app = Vue.createApp(ComponentsApp)
-//アプリケーション内のコンポーネントが使える色々を登録
-//同じインスタンスを返すのでチェーン可能
-    .component('SearchInput',SearchInputComponent)
-    .directive('focus',FocusDirective)
+    //アプリケーション内のコンポーネントが使える色々を登録
+    //同じインスタンスを返すのでチェーン可能
+    .component('SearchInput', SearchInputComponent)
+    .directive('focus', FocusDirective)
     .use(LocalePlugin)
 //data,props,methodsなどをコンポーネントオプション
 //ユーザ定義のプロパティを追加するオプション
-app.component('todo-item',{
-    props:['todo'],
-    template:`<li>{{todo.text}}</li>`
+app.component('todo-item', {
+    props: ['todo'],
+    template: `<li>{{todo.text}}</li>`
 })
 //mountはルートコンポーネントを返す
 app.mount('#components-app')
 
 Vue.createApp({
-    data(){
-        return{count:1}
+    data() {
+        return { count: 1 }
     },
     //ライフサイクルフック
     //createdフック：インスタンス作成後にコードを実行する。
-    created(){
-        console.log('count is:'+this.count)
+    created() {
+        console.log('count is:' + this.count)
     }
 })
 
