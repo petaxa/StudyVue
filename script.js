0fca8813612bb3b19091ff36cbcc2ce3066e1045
tempestif <soccer.i_y@icloud.com>
2021-09-21 05:18:01 +0900

    component

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const Counter = {
         },1000)
     }
 }
+Vue.createApp(Counter).mount('#counter')
+
 const AttributeBinding = {
     data(){
         return {
@@ -18,6 +20,8 @@ const AttributeBinding = {
         }
     }
 }
+Vue.createApp(AttributeBinding).mount('#bind-attribute')
+
 const EventHanding = {
     data(){
         return{
@@ -44,6 +48,78 @@ const EventHanding = {
         }
     }
 }
-Vue.createApp(Counter).mount('#counter')
-Vue.createApp(AttributeBinding).mount('#bind-attribute')
 Vue.createApp(EventHanding).mount('#event-handling')
+
+const ContidionalRendering = {
+    data(){
+        return{
+            seen:true
+        }
+    },
+    methods:{
+        toggleSeen(){
+            this.seen = !this.seen
+        }
+    }
+}
+Vue.createApp(ContidionalRendering).mount('#conditional-rendering')
+
+const ListRendering = {
+    data(){
+        return {
+            todos:[
+                {text:'Learn JS'},
+                {text:'Learn Vue'},
+                {text:'Build something awesome'},
+                {color:'red'},
+                {color:'blue'}
+            ]
+        }
+    }
+}
+Vue.createApp(ListRendering).mount('#list-rendering')
+
+const ComponentsApp = {
+    data(){
+        return{
+            groceryList:[
+                {id:0,text:'vegetables'},
+                {id:1,text:'cheese'},
+                {id:2,text:'whatever else humans are supposed to eat'}
+            ]
+        }
+    }
+}
+
+
+//アプリケーションとコンポーネント
+//アプリケーションインスタンスを作成
+const app = Vue.createApp(ComponentsApp)
+//アプリケーション内のコンポーネントが使える色々を登録
+//同じインスタンスを返すのでチェーン可能
+    .component('SearchInput',SearchInputComponent)
+    .directive('focus',FocusDirective)
+    .use(LocalePlugin)
+//data,props,methodsなどをコンポーネントオプション
+//ユーザ定義のプロパティを追加するオプション
+app.component('todo-item',{
+    props:['todo'],
+    template:`<li>{{todo.text}}</li>`
+})
+//mountはルートコンポーネントを返す
+app.mount('#components-app')
+
+Vue.createApp({
+    data(){
+        return{count:1}
+    },
+    //ライフサイクルフック
+    //createdフック：インスタンス作成後にコードを実行する。
+    created(){
+        console.log('count is:'+this.count)
+    }
+})
+
+
+
+
